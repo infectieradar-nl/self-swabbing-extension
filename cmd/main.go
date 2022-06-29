7f25ff219ca1e703996f6c1efa91ec122c12481e
phev8 <phev8@users.noreply.github.com>
2022-06-29 15:26:45 +0200

    add go.mod and initial main file

---
+++ cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/coneno/logger"
+
+func main() {
+	logger.Info.Println("Starting self-swabbing-extension")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/infectieradar-nl/self-swabbing-extension
+
+go 1.17
+
+require github.com/coneno/logger v1.2.1
